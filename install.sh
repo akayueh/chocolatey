f25fa251210fda3e935d7653316ee47edc2f6cd4
Steven Yu <akayueh@gmail.com>
2019-01-07 10:31:57 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install adobereader -y
-choco install googlechrome -y
-choco install notepadplusplus.install -y
+cinst adobereader -y
+cinst googlechrome -y
+cinst notepadplusplus.install -y
+cinst putty.install -y
+cinst winscp.install -y
+cinst teamviewer -y
+cinst postman -y
+cinst gimp -y
+cinst winmerge -y
